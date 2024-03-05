b05d061836776939bbe32dfb4a8f40504ec05885
Riya <kaushik.riya193@gmail.com>
2024-03-05 20:34:01 +0530

    improve tsconfig and fix Queryclient Proviuder error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Dashboard } from "./Components/Dashboard";
 import React from "react";
 
 function App() {
-  const queryClient = new QueryClient();
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <QueryClientProvider client={queryClient}>
-        <Dashboard />
-      </QueryClientProvider>
+    <div className="relative h-screen w-full flex items-center justify-center">
+      <Dashboard />
     </div>
   );
 }
